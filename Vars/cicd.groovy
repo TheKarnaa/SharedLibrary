6cfbb7b416153575b70ae4c61bac087dec04ccbe
TheKarnaa <136471181+TheKarnaa@users.noreply.github.com>
2023-09-04 17:50:39 +0530

    Update cicd.groovy

--- Vars/cicd.groovy
+++ Vars/cicd.groovy
@@ -7,3 +7,7 @@ def newBuild()
 {
   sh 'mvn package'
 }
+def neDeploy()
+{
+   sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
